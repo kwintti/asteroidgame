7aed7a5335ccb76f04d6b8db519023f7551a4847
kwintti <sergei.borovskii@gmail.com>
2024-09-21 22:24:40 +0300

    added fps 60 restrain

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        ret_ml = clock.tick(60)
+        dt = ret_ml / 1000
 
 if __name__ == "__main__":
     main()
